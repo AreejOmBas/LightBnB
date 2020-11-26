acacfd43e2bb8ebf4d5e17c6aa355d9c58ce83ff
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-26 16:49:14 +0000

    users login, signup functionality connected to db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -19,16 +19,11 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const values = [email];
+
+  const query = `SELECT * FROM users WHERE email = $1`;
+   return pool.query( query,values)
+  .then(res => res.rows[0] || null);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -38,7 +33,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const values = [id];
+
+  const query = `SELECT * FROM users WHERE id = $1`;
+   return pool.query( query,values)
+  .then(res => res.rows[0] || null);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -49,10 +48,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const {name, email, password} = user;
+  const values = [name , email, password];
+
+  const query = `INSERT INTO users(name,email,password) VALUES ($1,$2,$3) RETURNING *;`;
+   return pool.query( query,values)
+  .then(res => res.rows[0] || null);
 }
 exports.addUser = addUser;
 
@@ -77,7 +78,7 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  
+
   const values = [limit];
   const query = `SELECT * FROM properties LIMIT $1`;
   return pool.query( query,values)
